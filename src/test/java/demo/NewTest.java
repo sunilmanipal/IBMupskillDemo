e37673efee99f12108517c586d0ad30ac3a9e705
sunil <sunil.nagaraj@manipalglobal.com>
2019-01-09 16:19:27 +0530

    New project

---
+++ src/test/java/demo/NewTest.java
@@ -0,0 +1,22 @@
+package demo;
+
+import org.testng.annotations.Test;
+import org.testng.annotations.BeforeTest;
+import org.testng.annotations.AfterTest;
+
+public class NewTest {
+  @Test
+  public void f() {
+	  System.out.println("This is at test");
+  }
+  @BeforeTest
+  public void beforeTest() {
+	  System.out.println("This is before test");
+  }
+
+  @AfterTest
+  public void afterTest() {
+	  System.out.println("This is after test");
+  }
+
+}
